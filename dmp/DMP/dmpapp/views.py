a07f67930f5d39ebcf0c1d7b88f66ec753c5a4f6
Michel de Lange <michel_de_lange@yahoo.co.uk>
2015-06-10 13:59:11 +1200

    v

--- dmp/DMP/dmpapp/views.py
+++ dmp/DMP/dmpapp/views.py
@@ -1,7 +1,16 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from django.template.context import RequestContext
+
+from dmpapp.models import Project
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the DMP.")
+    project_list = Project.objects.all()
+    template = loader.get_template('dmpapp/index.html')
+    context = RequestContext(request, {
+        'project_list': project_list,
+    })
+    return HttpResponse(template.render(context))
