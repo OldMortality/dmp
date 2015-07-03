c9808372671d412172ed0d9344a8cc2971e9bf02
Michel de Lange <michel_de_lange@yahoo.co.uk>
2015-07-03 19:14:48 +1200

    fixed some things I screwed up earlier.

--- dmp/DMP/dmpapp/views.py
+++ dmp/DMP/dmpapp/views.py
@@ -76,8 +76,8 @@ class ProjectDetailView(LoggedInMixin,generic.DetailView):
 class DatasetView(LoggedInMixin,generic.ListView):
     
     model = Dataset
-    template_name = 'dmpapp/datasetlist.html'
-    context_object_name = 'dataset_list'
+    template_name = "dmpapp/datasetlist.html"
+    context_object_name = "dataset_list"
     
     def get_queryset(self):
         print("getting the projects")
@@ -86,13 +86,10 @@ class DatasetView(LoggedInMixin,generic.ListView):
                                       project__member__name=self.request.user)  
 
     
-    #def get_context_data(self, **kwargs):
-    #    # Call the base implementation first to get a context
-    #    context = super(DatasetView, self).get_context_data(**kwargs)
-    #    # Add in a QuerySet of all the books
-    #    context['project_id'] = self.request.GET.get("pid")
-    #   
-    #    return context
+    def get_context_data(self, **kwargs):
+        context = super(DatasetView, self).get_context_data(**kwargs)
+        context['project_id'] = self.request.GET.get("pid")
+        return context
     
             
 
